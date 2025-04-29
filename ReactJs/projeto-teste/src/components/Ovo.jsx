d572f64fd34863bc7d9a802252fea1cec6d4c406
Daniela <danielarodrigues2po@gmail.com>
2025-04-29 17:31:51 -0300

    Ovo rachando e bichinho

--- ReactJs/projeto-teste/src/components/Ovo.jsx
+++ ReactJs/projeto-teste/src/components/Ovo.jsx
@@ -1,11 +1,27 @@
 import './Ovo.css'
 import Ovo_normal from "../sprites/Ovo_normal.gif"
+import Ovo_rachando from "../sprites/Ovo_rachando.gif"
+import { useState } from 'react';
 
 function Ovo(){
 
+    const [sprite, setSprite] = useState(Ovo_normal);
+    const [cliques, setCliques] = useState(0);
+
+    const verificaCliques = () =>{
+
+        const contagem = cliques + 1;
+        setCliques(contagem);
+
+        if (contagem >= 25){
+            setSprite(Ovo_rachando)
+            
+        }
+    }
+
     return(
         <div>
-            <img src={Ovo_normal} alt="Ovo" className="Ovo_normal"/>
+            <img src={sprite} alt="Ovo" className="Ovo_normal" onClick={verificaCliques}/>
         </div>
         
     );
