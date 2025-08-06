ec3fab379e90634669f4476ef6906e05005c8e61
Daniela <danielarodrigues2po@gmail.com>
2025-08-06 14:57:24 -0300

    Conexão com MongoDb realizada

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -6,8 +6,12 @@ mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
 })
-
-
-app.listen(3000, () => {
-    console.log('Server Ligado')
+.then(() =>{
+    console.log('Conexão com MongoBd foi feita :3');
+    app.listen(3000, () => {
+        console.log('Servidor foi ligado na porta 3000')
+    });
+})
+.catch((err) =>{
+    console.error('Erro ao se conectar com o MongoDb ;-;,', err.message);
 })
