d053ef071fa7d718869d54e59bbbb011dd02131f
Daniela <danielarodrigues2po@gmail.com>
2025-08-06 15:58:33 -0300

    esqueleto tela de login

---
+++ ReactJs/Front-End/src/components/Login.jsx
@@ -0,0 +1,25 @@
+import './Login.css'
+import { useState } from 'react'
+
+function Login(){
+
+    return(
+
+        <div className='Login'>
+            <div className='email'>
+
+            </div>
+            <div className='senha'>
+
+            </div>
+            <div className='Botao'>
+                
+            </div>
+        </div>
+
+    )
+
+
+}
+
+export default Login
