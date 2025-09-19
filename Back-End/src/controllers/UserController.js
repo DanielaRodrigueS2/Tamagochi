79b1b71523a7e79c072901f600582abced170325
Daniela <danielarodrigues2po@gmail.com>
2025-09-19 19:40:14 -0300

    User Controller

--- Back-End/src/controllers/UserController.js
+++ Back-End/src/controllers/UserController.js
@@ -1,7 +1,32 @@
 const User = require('../models/UserModel');
+const Tamagochi = require('../models/TamagochiModel');
 
-exports.updateUser = async (req, res) =>{
-    const {nome, email, senha} = req.body;
-    
+exports.getUser = async (req, res) =>{
+    const uid = req.params.id;
+    try{
+        const user = await User.findById(uid);
+        if(!user){
+            res.status(404).json({erro: 'Erro ao encontrar tamagochi'});
+        }
+        res.json(user);
+    }
+    catch(erro){
+        res.status(500).json({erro: 'Erro ao retonar user'});
+    }
+
+}
+
+exports.getTamagochiByUserId = async (req, res) =>{
+    const responsavel = req.params.id;
+    try{
+        const tamagochi = await Tamagochi.findOne({responsavel});
+        if(!tamagochi){
+            res.status(404).json({erro: 'Tamagochi não encontrado'});
+        }
+        res.json(tamagochi)
+    }
+    catch(erro){
+
+    }
 
 }
