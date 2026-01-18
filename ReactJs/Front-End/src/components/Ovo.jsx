df76a22ffcb71722187d108348bcbe2bdfc6cbf5
Daniela <danielarodrigues2po@gmail.com>
2026-01-18 20:49:49 -0300

    implementação Redux no objeto Ovo

--- ReactJs/Front-End/src/components/Ovo.jsx
+++ ReactJs/Front-End/src/components/Ovo.jsx
@@ -3,18 +3,19 @@ import Ovo_normal from "../sprites/Ovos/Ovo_normal.gif"
 import Ovo_rachando from "../sprites/Ovos/Ovo_rachando.gif"
 import tubaraoparado from "../sprites/Tubarao/tubaraoparado.gif"
 import { useDroppable } from '@dnd-kit/core';
-import React from 'react';
 import { useState } from 'react';
+import { useSelector, useDispatch} from 'react-redux'
+import { usarItem, alterarSprite, incrementar } from '../redux/tamagochiSlice';
 
 function Ovo({ultimoItem}){
 
-    const [sprite, setSprite] = useState(Ovo_normal);
-    const [cliques, setCliques] = useState(0);
+    //const [sprite, setSprite] = useState(Ovo_normal);
+    //const [cliques, setCliques] = useState(0);
+    const dispatch = useDispatch();
+    const cliques = useSelector(state => state.tamagochi.cliques);
+    const sprite = useSelector(state => state.tamagochi.sprite)
 
     const verificaCliques = () =>{
-
-        const contagem = cliques + 1;
-        setCliques(contagem);
 
         if (contagem >= 25 && sprite !== tubaraoparado){
             setSprite(Ovo_rachando);
