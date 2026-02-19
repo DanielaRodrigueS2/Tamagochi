62f1a73c3acc0cca1738ac46ce1f6d39240864d8
Daniela <danielarodrigues2po@gmail.com>
2026-02-18 21:36:14 -0300

    opção salvar tamagochi

--- ReactJs/Front-End/src/components/MenuConfig.jsx
+++ ReactJs/Front-End/src/components/MenuConfig.jsx
@@ -1,16 +1,42 @@
+import { useState } from 'react';
 import './MenuConfig.css'
+import { useSelector } from 'react-redux';
 
 function MenuConfig(props){
+
+    const tamagochi = useSelector(state => state.tamagochi);
 
     const menuLogin = () =>{
         props.setMenu(false);
         props.setMenuLog(true);
     }
 
+    const updateTamagochi = () =>{
+        console.log(tamagochi);
+        try{
+            fetch(`http://3000/tamagochi`,{
+                method: 'PATCH',
+                headers:{
+                    'Content-Type' : 'applications/json',
+                    'Authorization' : `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify(tamagochi)
+            });
+
+            console.log('Salvo com sucesso');
+
+        }
+            catch(error){
+                console.log('Erro ao salvar')
+        }
+
+
+    }
+
     return(
         <div className='menuConfig'>
             <h1>Configurações</h1>
-            <button>Salvar</button>
+            <button onClick={updateTamagochi}>Salvar</button>
             <button>Reiniciar</button>
             <button onClick={menuLogin}>Tela de Login</button>
             <button onClick={() => props.setMenu(false)}>Fechar Menu</button>
