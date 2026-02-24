3e42587cbdc1f4ec336929ce1205c403d57edca5
Daniela <danielarodrigues2po@gmail.com>
2026-02-24 19:37:02 -0300

    Add cores dinamicas para as barras de status

--- ReactJs/Front-End/src/components/Status.jsx
+++ ReactJs/Front-End/src/components/Status.jsx
@@ -2,13 +2,19 @@ import './Status.css'
 import { useSelector } from 'react-redux';
 import { SlEnergy } from "react-icons/sl";
 import { FaFish, FaGrin} from "react-icons/fa";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Status(props){
     
     const fome = useSelector(state => state.tamagochi.fome);
     const energia = useSelector(state => state.tamagochi.energia)
     const alegria = useSelector(state => state.tamagochi.felicidade)
+
+    const retornaCor = (valor) =>{
+        if (valor >= 70) return 'green';
+        if (valor < 70 && valor >= 30) return 'orange';
+        return 'red';
+    }
 
 
     useEffect(() =>{
@@ -19,15 +25,15 @@ function Status(props){
         <div className='Status'>
             <div className='barras'>
                 <FaFish size={19} className='Icon'/>
-                <progress className='barra' value={fome} max='100'></progress>
+                <progress style={{accentColor: retornaCor(fome)}} className='barra' value={fome} max='100'></progress>
             </div>
             <div className='barras'>
                 <SlEnergy size={19} className='Icon'/>
-                <progress className='barra' value={energia} max='100'></progress>
+                <progress style={{accentColor: retornaCor(energia)}}  className='barra' value={energia} max='100'></progress>
             </div>
             <div className='barras'>
                 <FaGrin size={19} className='Icon'/>
-                <progress className='barra' value={alegria} max='100'></progress>
+                <progress  style={{accentColor: retornaCor(alegria)}} className='barra' value={alegria} max='100'></progress>
             </div>
         </div>
     );
