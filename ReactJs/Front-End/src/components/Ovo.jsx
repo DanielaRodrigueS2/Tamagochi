e1b303209f99ce0183616f8a76a3c6d93b794c78
Daniela <danielarodrigues2po@gmail.com>
2026-02-23 21:01:21 -0300

    salvamento automatico implementado

--- ReactJs/Front-End/src/components/Ovo.jsx
+++ ReactJs/Front-End/src/components/Ovo.jsx
@@ -7,8 +7,10 @@ import { usarItem, alterarSprite, incrementar } from '../redux/tamagochiSlice';
 function Ovo(props){
 
     const dispatch = useDispatch();
+    const tamagochi = useSelector(state => state.tamagochi);
     const cliques = useSelector(state => state.tamagochi.cliques);
     const sprite = useSelector(state => state.tamagochi.sprite);
+    const [intervalo, setIntervalo] = useState(60000);
 
     const verificaCliques = () =>{
         dispatch(incrementar());
@@ -41,14 +43,58 @@ function Ovo(props){
         }
     }, [props.ultimoItem])
 
- 
+
     // Drag and drop
     const {isOver, setNodeRef} = useDroppable({
         id: 'tamagochi'
     });
     const style ={
         color: isOver ? 'green' :undefined,
-    };
+    }; 
+
+    // Função de update automatico
+    const updateTamagochi = async () =>{
+        try{
+            const resposta = await fetch(`http://localhost:3000/tamagochi`,{
+                method: 'PUT',
+                headers:{
+                    'Content-Type' : 'application/json',
+                    'Authorization' : `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify(tamagochi)});
+
+            const data = await resposta.json();
+            console.log(resposta);
+            console.log('Salvo com sucesso');
+
+        }
+        catch(error){
+            console.log('Erro ao salvar', error);
+        }
+
+
+    }
+
+    // Use Effect para salvamento automatico
+    useEffect(() =>{
+        
+        const primeiraFuncao = () =>{
+            updateTamagochi();
+            novoIntervalo();
+        }
+
+        const timer = setTimeout(primeiraFuncao, intervalo);
+
+        return () => clearTimeout(timer);
+        
+    },[intervalo]);
+
+    // Funcao para resetar intervalo
+    const novoIntervalo = () =>{
+
+        const intervaloNovo = Math.floor(Math.random() * 500) + 60000;
+        setIntervalo(intervaloNovo);
+    }
  
 
     return(
