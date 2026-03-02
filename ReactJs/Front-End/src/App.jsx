9165edc1a5545d9a984f3120fdd1250871d57509
Daniela <danielarodrigues2po@gmail.com>
2026-03-02 15:40:42 -0300

    Utilização de Browser Router + criação pagina ResetPassword

--- ReactJs/Front-End/src/App.jsx
+++ ReactJs/Front-End/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ResetPassword from './pages/ResetPassword'
 import Home from "./pages/Home";
 import './App.css'
 
 function App(){
   
   return(
-    <Home></Home>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/passwordReset' element={<ResetPassword/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 
 }
