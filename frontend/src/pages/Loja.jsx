e625b74c81e189a58d9b08385e87bf956ada68f9
Daniela <danielarodrigues2po@gmail.com>
2026-03-12 18:43:27 -0300

    Implementação da loja (estrutura css completa)

--- frontend/src/pages/Loja.jsx
+++ frontend/src/pages/Loja.jsx
@@ -1,18 +1,38 @@
 import "./Loja.css"
+import { useState } from "react"
 
 function Loja(){
+
+    const [valor, setValor] = useState(0)
+
     return(
         <div className="Loja">
-            <header>
 
-            </header>
+            <header>
+                <div className="header-text">
+                    <p>Loja de itens</p>
+                </div>
+
+                <div className="dinheiro">
+                    <img src="extras/melancia.gif" className="img-melancia"></img>
+                    <p>{valor}</p>
+                </div>
+            </header>   
 
             <main>
+                <div className="categorias">
+                    <div>
 
+                    </div>
+                </div>
+
+                <div className="main-itens"></div>
             </main>
 
             <footer>
-                
+                <img></img>
+                <button className="botao-voltar">Voltar</button>
+                <img></img>
             </footer>
         </div>
     )
